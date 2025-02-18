3876379ae4467c3a3ea100ea20ec771f9ce52144
Angga Islami <84172981+AnggaaIs@users.noreply.github.com>
2025-02-18 11:29:03 +0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -79,7 +79,7 @@
 		await checkAll();
 
 		try {
-			const res = await fetch(`/api/data?prodi=RPL&kelas=1A`);
+			const res = await fetch(`/api/data?prodi=RPL&kelas=2A`);
 			if (!res.ok) throw new Error("Gagal mengambil data");
 
 			const result: ScheduleResponse = await res.json();
